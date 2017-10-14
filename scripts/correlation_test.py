a39fd14d6f9c08c1245938a064549221222a5006
Maurice Wong <maurice.wong24@gmail.com>
2017-10-14 17:44:37 -0400

    Randomized scores chisq distribution test initial

--- scripts/correlation_test.py
+++ scripts/correlation_test.py
@@ -5,11 +5,44 @@ import json
 from random import shuffle
 from itertools import combinations
 
+RAND_NUM = 100
 
 def main():
-    restaurant_user_ratings_map = read_restaurant_by_user_ratings()
+    rest_user_ratings_map = read_restaurant_by_user_ratings()
     friend_graph = init_friend_graph('../data/yelpFriends.txt')
-    
+
+    # A list of lists, where each list corresponds to a restaurant subgraph
+    # Each list contains RAND_NUM chi sq values generated from permuting the graph
+    random_chisq_values = []
+
+    # Running sum of the chi sq values for each restaurant of the real graph
+    real_chisq_sum = 0
+
+    for r_id in rest_user_ratings_map:
+        user_ratings = rest_user_ratings_map[r_id]
+
+        # generate random chisq values first:
+        rand_user_ratings = get_random_score_ratings(user_ratings, RAND_NUM)
+        chisq_vals = [calc_chi_sq(x, friend_graph) for x in rand_user_ratings]
+        random_chisq_values.append(chisq_vals)
+
+        # calculate chisq value for actual graph
+        real_chisq_sum += calc_chi_sq(dict(user_ratings), friend_graph)
+
+    # list of chisq sums for RAND_NUM trials
+    sum_rand_chisq = [sum(i) for i in zip(*random_chisq_values)]
+    sum_rand_chisq.sort()
+
+    print(sum_rand_chisq)
+    print(real_chisq_sum)
+    # calculate what percentile the real sum is in compared to the distribution of random
+    percentile = 1
+    for i in range(len(sum_rand_chisq)):
+        if sum_rand_chisq[i] > real_chisq_sum:
+            percentile = i/len(sum_rand_chisq)
+            break
+    print("Percentile: " + str(percentile))
+
 
 
 def read_restaurant_by_user_ratings():
@@ -40,7 +73,8 @@ def get_random_score_ratings(user_ratings_list, num):
 def calc_chi_sq(user_ratings_map, graph):
     '''
     Takes a mapping from user_id => ratings and adjacency list graph. Calculates
-    the chi square value
+    the chi square value. If an exp value is 0, returns 0 since the exp value
+    will be 0 for all permutations of graphs/scores
     '''
     users = list(user_ratings_map.keys())
     user_pairs = list(combinations(users, 2))
@@ -61,6 +95,8 @@ def calc_chi_sq(user_ratings_map, graph):
                 d += 1
     num = ((a*d-c*b)**2) * (a+b+c+d)
     denom = (a+b) * (c+d) * (b+d) * (a+c)
+    if denom == 0:
+        return 0
     return num/denom
 
 
