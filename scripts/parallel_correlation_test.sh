b845de76799bfc224e5b7fb423131da5c3fb2374
Maurice Wong <maurice.wong24@gmail.com>
2017-11-25 23:02:49 -0500

    time script

--- scripts/parallel_correlation_test.sh
+++ scripts/parallel_correlation_test.sh
@@ -1,3 +1,4 @@
+start=`date +%s`
 python3 correlation_test.py score | head -250 > score_1.txt &
 python3 correlation_test.py score | head -250 > score_2.txt &
 python3 correlation_test.py score | head -250 > score_3.txt &
@@ -11,3 +12,6 @@ python3 correlation_test.py edge | head -250 > edge_4.txt &
 wait
 cat score_1.txt score_2.txt score_3.txt score_4.txt | sort -n > all_scores.txt
 cat edge_1.txt edge_2.txt edge_3.txt edge_4.txt | sort -n > all_edges.txt
+end=`date +%s`
+
+echo $((end-start))
