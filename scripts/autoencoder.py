3d215e94b3181ee7b95bccc06996a36a3eab7c96
Maurice Wong <maurice.wong24@gmail.com>
2018-03-02 01:08:39 -0500

    Loss and Models created (untested)

--- scripts/autoencoder.py
+++ scripts/autoencoder.py
@@ -1,124 +1,107 @@
 import torch
-import torch.autograd as autograd
+from torch.autograd import Variable
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 
-N = 10 # number of vectors in the data set, i.e. |V|+|W|
-D = 3 # number of dimensions in the encoded vectors
-
-class L1EmbeddingLoss(nn.Module):
-	def __init__(self, friend_sim_matrix):
-		super(L1EmbeddingLoss, self).__init__()
-		self.friend_sim_matrix = friend_sim_matrix
+D_OUT = 3 # number of dimensions in the encoded vectors
+H1_DIM = 64 # dimension of first hidden layer
+H2_DIM = 12 # dimension of second hidden layer
 
 
-	def cross_difference(inputs):
-		'''
-		Given a list of 1D tensors (2D tensor batch), does the pair wise combination
-		of each 1D tensor and finds their difference. Outputs a 2D tensor of differences.
-		inputs is a 2D tensor batch where each row is a 1D tensor for the cross difference
-		'''
-		n = inputs.size()[0]
-		res = tensor.zeros(n,n)
-		for i in range(n):
-			for j in range(n):
-				res[i][j] = F.cosine_similarity(inputs[i], inputs[j])
-		return res
+class SimMatrixEmbeddingLoss(nn.Module):
+	def __init__(self, sim_matrix):
+		super(SimMatrixEmbeddingLoss, self).__init__()
+		self.sim_matrix = sim_matrix
 
 
-	def forward(self, inputs):
-		'''
-		Takes the input and performs a cartesian product with itself to compute
-		loss on pairs of the input based on the similarity matrix
-		'''
-		diffs = cross_difference(inputs)
-		return torch.sum((self.friend_sim_matrix - diffs)**2)
-
-
-
-class WordEmbeddingLoss(nn.Module):
-	def __init__(self, word_sim_matrix):
-		super(WordEmbeddingLoss, self).__init__()
-		self.word_sim_matrix = word_sim_matrix
-
-
-	def cross_difference(users, words):
+	def cross_difference(input1, input2):
 		'''
 		users - 2D tensor of dimension V x P
 		words - 2D tensor of dimension W x P
 		Computes the cosine similarity of each pairwise combination of users and word 1D tensors
 		Returns a resulting matrix of dimension V x W
 		'''
-		v = users.size()[0]
-		w = words.size()[0]
-		res = np.zeros(v, w)
-		for i in range(v):
-			for j in range(w):
-				res[i][j] = F.cosine_similarity(users[i], words[j])
+		u = input1.size()[0]
+		v = input2.size()[0]
+		res = np.zeros(u, v)
+		for i in range(u):
+			for j in range(v):
+				res[i][j] = F.cosine_similarity(input1[i], input2[j])
 		return res
 
 
-	def forward(self, users, words):
+	def forward(self, input1, input2):
 		'''
 		Takes users and words, computes cosine similarity between them and compares them to
 		word-user similarity matrix to compute the loss
 		'''
-		cross_diffs = cross_difference(users, words)
-		return torch.sum((self.word_sim_matrix - cross_diffs)**2)
-
+		cross_diffs = cross_difference(input1, input2)
+		return torch.sum((self.sim_matrix - cross_diffs)**2)
 
 
 class DeepAutoencoder(nn.Module):
-	def __init__(self):
+	def __init__(self, D_in, H1, H2, D_out):
 		super(DeepAutoencoder, self).__init__()
 		self.encoder = nn.Sequential(
-			nn.Linear(INPUT_VECTOR_SIZE, 64),
+			nn.Linear(D_in, H1),
 			nn.ReLU(True),
-			nn.Linear(64, 12),
+			nn.Linear(H1, H2),
 			nn.ReLU(True),
-			nn.Linear(12, 3),
+			nn.Linear(H2, D_out),
 			nn.ReLU(True)
 			)
 		self.decoder = nn.Sequential(
-			nn.Linear(3, 12),
+			nn.Linear(D_out, H2),
 			nn.ReLU(True),
-			nn.Linear(12, 64),
+			nn.Linear(H2, H1),
 			nn.ReLU(True),
-			nn.Linear(64, INPUT_VECTOR_SIZE),
+			nn.Linear(H1, D_in),
 			nn.ReLU(),
 			)
+
 
 	def forward(self, x):
 		y = self.encoder(x)
 		#x_pred = self.decoder(x)
-
 		return y
 
-#input matrix, randomly initialized for now
-X = torch.randn(N, N)
-#randomly initialize the ouput matrix
-Y = torch.randn(N, D)
 
-#we will keep track of the loss at each iteration
+# random Tensors to hold inputs
+X_user = Variable(torch.randn(N, D_in))
+X_word = Variable(torch.randn(N, D_in))
+
+# We will keep track of the loss at each iteration
 losses = []
-#initialize the model and the optimizer
+
+# Initialize the model and the optimizer
+#TODO feed in word dimensions
+#TODO feed in user dimensions
+model_user = DeepAutoencoder(user_num, H1_DIM, H2_DIM, D_OUT)
+model_word = DeepAutoencoder(word_num, H1_DIM, H2_DIM, D_OUT)
+model_parameters = [model_word, model_user]
+
 #Adam outperforms stochastic gradient descent
-model = DeepAutoencoder(nn.Module):
-optimizer = optim.Adam(model.parameters(), lr=0.001)
+optimizer = optim.Adam(model_parameters, lr=0.001)
+
+# Construct our loss functions:
+#TODO friend matrix??
+#TODO user_word_matrix??
+friend_criterion = SimMatrixEmbeddingLoss(friend_matrix)
+word_criterion = SimMatrixEmbeddingLoss(user_word_matrix)
 
 for epoch in range(100):
 	total_loss = 0
-	for i in range(N):
-		#get a copy of the current row in a Variable object
-		x_i = autograd.Variable(X[i].clone())
-		y_pred = model(x_i)
+	y_user_pred = model_user(X_user)
+	y_word_pred = model_word(X_word)
 
-		model.zero_grad()
+	optimizer.zero_grad()
 
-		loss = loss_function(y_pred)
+	friend_loss = friend_criterion(y_user_pred, y_user_pred)
+	user_word_loss = word_criterion(y_user_pred, y_word_pred)
+	loss = friend_loss + user_word_loss
 
-		total_loss += loss
+	total_loss += loss
 
 	loss.backward()
 	optimizer.step()
