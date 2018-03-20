2bb314d6d4b68d5faab93f526aabb710b81f59d7
Maurice Wong <maurice.wong24@gmail.com>
2018-03-19 22:38:46 -0400

    friend list and eliminated redundant directions

--- scripts/friend_edge_list.py
+++ scripts/friend_edge_list.py
@@ -10,14 +10,15 @@ def main():
     index_to_user = list(friend_graph.keys())
     user_to_index = {k: v for v, k in enumerate(index_to_user)}
     V = len(index_to_user)
-    edges = []
+    edges = set()
     for user1, friend_list in friend_graph.items():
         for user2 in friend_list:
             idx1 = user_to_index.get(user1)
             idx2 = user_to_index.get(user2)
             if (idx1 is not None and idx2 is not None):
-                edges.append((idx1, idx2))
+                edges.add(tuple(sorted([idx1, idx2])))
 
+    edges = sorted(edges)
     with open('../data/friend_edge_list.txt', 'w') as f:
         for edge in edges:
             f.write(str(edge[0]) + " " + str(edge[1]) + "\n")
